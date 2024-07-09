f1fd141b88990787209bb890c20936e6e909ada5
macbook m1 <dungdv01852@gmail.com>
2024-07-09 08:46:43 +0700

    vdsv

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import * as Bluetooth from 'react-bluetooth';
+import { Link, Route, Routes } from 'react-router-dom';
+import { Home } from './screen/Home';
+import { AboutUs } from './screen/AboutUs';
 
 function App() {
+
+  useEffect(() => {
+
+  }, [])
+
+  const connect = async () => {
+    const isAvailable = await Bluetooth.getAvailabilityAsync();
+    if (!isAvailable) {
+      return;
+    }
+
+    const options = {
+      filters: [{ services: ['battery_service'] }],
+    };
+
+    try {
+      const result = await Bluetooth.requestDeviceAsync(options);
+      if (result.type === 'cancel') {
+        return;
+      }
+      const { device } = result;
+
+      console.log(`Bluetooth: Got device:`, device);
+      if (device.gatt) {
+        const server = await device.gatt.connect();
+        console.log(`Bluetooth: Got server:`, server);
+        const service = await server.getPrimaryService('battery_service');
+        console.log(`Bluetooth: Got service:`, service);
+        const characteristic = await service.getCharacteristic('battery_level');
+        console.log(`Bluetooth: Got characteristic:`, characteristic);
+        const value = await characteristic.readValue();
+        console.log(`Bluetooth: Got value:`, value);
+        const battery = value.getUint8(0);
+        console.log(`Success: Got battery:`, battery);
+      } else {
+        // TODO: Bacon: Can we connect to the GATT or is that a no-op?
+        console.error(`Error: connected device did not have a GATT`);
+      }
+    } catch ({ message }) {
+      console.error(`Error: Couldn't get battery level: ${message}`);
+    }
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div style={{
+          display: 'flex',
+          justifyContent: 'space-evenly',
+          height: 200,
+          backgroundColor: 'green',
+          fontSize: '20px',
+        }}>
+          <Link to={'/'}
+            style={{ color: 'red' }}>
+            Home
+          </Link>
+          <Link to={'/about'}
+            style={{ color: 'orange' }}>
+            About
+          </Link>
+        </div>
+      </div>
+      <Routes>
+        <Route path="/"
+          element={<Home />} />
+        <Route path="/about"
+          element={<AboutUs />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/screen/AboutUs.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export const AboutUs = () => {
+    return (
+        <div>
+            This is about us page
+        </div>
+    )
+}
